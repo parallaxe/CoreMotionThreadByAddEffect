d3b6d4a8db6f0b8e68063179186974b19a4ad45f
Hendrik von Prince <hendrik.von.prince@sipeso.com>
2017-06-07 12:19:17 +0200

    Basic example that calling addMotionEffect: activates a CoreMotion-thread that will increase the frequency in which the runloop is triggered while the device gets moved.

--- CoreMotionThreadByAddEffect/ViewController.m
+++ CoreMotionThreadByAddEffect/ViewController.m
@@ -9,21 +9,84 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIView *fixedView;
+@property (strong, nonatomic) IBOutlet UIView *floatingView;
+@property (strong, nonatomic) IBOutlet UILabel *runloopCounterLabel;
+@property(nonatomic) NSInteger runloopCounter;
+@property(nonatomic) CFRunLoopObserverRef runloopObserver;
 
+-(void)incrementCounter;
 @end
+
+
+void runloopCallback(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info) {
+    NSLog(@"runloop %lu", activity);
+    ViewController *controller = (__bridge ViewController *)info;
+    [controller incrementCounter];
+    switch(activity) {
+        case kCFRunLoopExit:
+            NSLog(@"  kCFRunLoopExit");
+            break;
+        case kCFRunLoopEntry:
+            NSLog(@"  kCFRunLoopEntry");
+            break;
+        case kCFRunLoopAfterWaiting:
+            NSLog(@"  kCFRunLoopAfterWaiting");
+            break;
+        case kCFRunLoopBeforeTimers:
+            NSLog(@"  kCFRunLoopBeforeTimers");
+            break;
+        case kCFRunLoopAllActivities:
+            NSLog(@"  kCFRunLoopAllActivities");
+            break;
+        case kCFRunLoopBeforeSources:
+            NSLog(@"  kCFRunLoopBeforeSources");
+            break;
+        case kCFRunLoopBeforeWaiting:
+            NSLog(@"  kCFRunLoopBeforeWaiting");
+            break;
+    }
+}
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.floatingView.layer.borderColor = [UIColor blackColor].CGColor;
+    self.floatingView.layer.borderWidth = 1;
+    self.fixedView.layer.borderColor = [UIColor blackColor].CGColor;
+    self.fixedView.layer.borderWidth = 1;
+
+    CFRunLoopObserverContext context;
+    context.version = 0;
+    context.info = (__bridge void *)(self);
+    context.release = NULL;
+    context.retain = NULL;
+    context.copyDescription = NULL;
+    self.runloopObserver = CFRunLoopObserverCreate(NULL, kCFRunLoopAllActivities, true, 0, &runloopCallback, &context);
+
+    CFRunLoopAddObserver(CFRunLoopGetMain(), self.runloopObserver, kCFRunLoopCommonModes);
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)incrementCounter {
+    ++self.runloopCounter;
+    self.runloopCounterLabel.text = [NSString stringWithFormat:@"%d", self.runloopCounter];
 }
 
+- (IBAction)activateCoreMotion:(id)sender {
+    UIInterpolatingMotionEffect *effectX = [[UIInterpolatingMotionEffect alloc] initWithKeyPath: @"center.x" type: UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    effectX.minimumRelativeValue = @(-30);
+    effectX.maximumRelativeValue = @(30);
+    
+    UIInterpolatingMotionEffect *effectY = [[UIInterpolatingMotionEffect alloc] initWithKeyPath: @"center.y" type: UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    effectY.minimumRelativeValue = @(-30);
+    effectY.maximumRelativeValue = @(30);
+    
+    UIMotionEffectGroup *effectGroup = [[UIMotionEffectGroup alloc] init];
+    effectGroup.motionEffects = @[effectX, effectY];
+    [self.floatingView addMotionEffect:effectGroup];
+}
 
 @end
