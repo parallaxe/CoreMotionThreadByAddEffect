d869c59ffadc51612db8bf4294371848332a556e
Hendrik von Prince <hendrik.von.prince@sipeso.com>
2017-06-07 11:47:03 +0200

    Initial Commit

---
+++ CoreMotionThreadByAddEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreMotionThreadByAddEffect
+//
+//  Created by Hendrik von Prince on 07.06.17.
+//  Copyright © 2017 Hendrik von Prince. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreMotionThreadByAddEffect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreMotionThreadByAddEffect
+//
+//  Created by Hendrik von Prince on 07.06.17.
+//  Copyright © 2017 Hendrik von Prince. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
